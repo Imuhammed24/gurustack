f475a78bfa3852efe0e12b2a3eda01d93b893850
imuhammed <ishaqmuhammed191@gmail.com>
2019-12-26 14:36:02 +0100

    Gurustack project begining

--- gurustack/settings.py
+++ gurustack/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = '34y7pe!02a3r)tsv$#utyn0%e%f+z_&it$qr#ssw7pki3$cd3w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.43.223', '127.0.0.1'
+]
 
 
 # Application definition
